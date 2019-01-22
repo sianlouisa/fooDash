84f638a7286baa9a7010bec62ad99a48d0ec14b4
Nawaar Patel <nawaarpatel@Nawaars-MacBook-Pro.local>
2019-01-22 17:52:50 +0000

    Completed sign up to firebase - need local config file

--- App.js
+++ App.js
@@ -1,35 +1,48 @@
 import React, { Component } from 'react';
 import { ViroARSceneNavigator } from 'react-viro';
 import VIRO_API_KEY from './config.js';
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import Login from './js/Components/Login';
+import Signup from './js/Components/Signup.js';
+// let sharedProps = {
+//   apiKey: VIRO_API_KEY,
+// };
 
-let sharedProps = {
-  apiKey: VIRO_API_KEY,
-};
+// let InitialARScene = require('./js/ARView');
 
-let InitialARScene = require('./js/ARView');
+// export default class App extends Component {
+
+//     state = {
+//       sharedProps: sharedProps,
+//     };
+
+//   render() {
+//     return this._getARNavigator();
+//   }
+
+//   _getARNavigator = () => {
+//     return (
+//       <ViroARSceneNavigator
+//         {...this.state.sharedProps}
+//         initialScene={{ scene: InitialARScene }}
+//       />
+//     );
+//   }
+// }
+
+// module.exports = App;
 
 export default class App extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      sharedProps: sharedProps,
-    };
-    this._getARNavigator = this._getARNavigator.bind(this);
-  }
+  state = {};
 
   render() {
-    return this._getARNavigator();
-  }
-
-  _getARNavigator() {
-    return (
-      <ViroARSceneNavigator
-        {...this.state.sharedProps}
-        initialScene={{ scene: InitialARScene }}
-      />
-    );
+    return <SwitchNavContainer />;
   }
 }
 
-module.exports = App;
+const SwitchNav = createSwitchNavigator({
+  Signup: Signup,
+  Login: Login,
+});
+
+const SwitchNavContainer = createAppContainer(SwitchNav);
