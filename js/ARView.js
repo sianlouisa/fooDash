ffc30039586a86088082ed872eb3628b59a847b2
SianLouisa <sianlouisa@hotmail.com>
2019-01-23 16:51:10 +0000

    added rule to change error of no-return-assign to warning & render before funcs

--- js/ARView.js
+++ js/ARView.js
@@ -1,78 +1,125 @@
 import React, { Component } from 'react';
 import {
   ViroARScene,
-  ViroConstants,
-  ViroARPlaneSelector,
   ViroAmbientLight,
-  ViroBox,
   ViroMaterials,
   Viro3DObject,
   ViroNode,
-  ViroLightingEnvironment,
   ViroQuad,
   ViroARPlane,
-  ViroText,
   ViroSphere,
   ViroARCamera
 } from 'react-viro';
-
+import TimerMixin from 'react-timer-mixin';
 import smile from './res/res/emoji_smile/emoji_smile.vrx';
 import diffuse from './res/res/emoji_smile/emoji_smile_diffuse.png';
 import normal from './res/res/emoji_smile/emoji_smile_normal.png';
 import specular from './res/res/emoji_smile/emoji_smile_specular.png';
-import planeDiffuse from './res/textures/Metal_grunge_001_COLOR.jpg';
-import planeNormal from './res/textures/Metal_grunge_001_NRM.jpg';
-import planeSpecular from './res/textures/Metal_grunge_001_SPEC.jpg';
 
 export default class ARView extends Component {
   state = {
-    isTracking: false,
-    initialized: false,
-    planeWidth: 1,
-    planeLength: 1,
-    planeCenter: [0, 0, 0],
-    planePosition: [0, 0, 0],
-    planeRotation: [0, 0, 0],
-    foundPlane: false,
-    quadRendered: false
+    planePosition: [0, 0, 0]
   };
 
+  render() {
+    const { planePosition } = this.state;
+
+    return (
+      <>
+        <ViroARScene
+          onTrackingUpdated={this.onInitialized}
+          physicsWorld={{
+            gravity: [0, -9.81, 0]
+          }}
+        >
+          <ViroAmbientLight color="#ffffff" />
+          <ViroARPlane
+            onPlaneSelected={this.onPlaneSelected}
+            ref={component => (this.arPlaneRef = component)}
+          >
+            <ViroNode position={planePosition}>
+              <ViroQuad
+                position={[0, 0, 0]}
+                scale={[1.5, 1.5, 1.5]}
+                rotation={[-90, 0, 0]}
+                physicsBody={{ type: 'Static', restitution: 0.75, friction: 0.7 }}
+                materials="ground"
+                renderingOrder={-1}
+              />
+              <ViroQuad
+                key="deadSpace"
+                onCollision={this.resetPlayer}
+                height={100}
+                width={100}
+                rotation={[-90, 0, 0]}
+                position={[0, -1, 0]}
+                materials={['transparent']}
+                physicsBody={{ type: 'Static' }}
+              />
+              <ViroNode position={planePosition}>{this.generatePlayer()}</ViroNode>
+            </ViroNode>
+          </ViroARPlane>
+
+          <ViroARCamera active position={[0, 0, 0]}>
+            <ViroSphere
+              onClick={this.handleLeftClick}
+              position={[-0.04, -0.05, -0.2]}
+              materials={['spherematerial']}
+              scale={[0.01, 0.01, 0.01]}
+            />
+            <ViroSphere
+              onClick={this.handleRightClick}
+              position={[-0.04, -0.1, -0.2]}
+              materials={['spherematerial']}
+              scale={[0.01, 0.01, 0.01]}
+            />
+            <ViroSphere
+              onClick={this.handleDownClick}
+              position={[-0.055, -0.075, -0.2]}
+              materials={['spherematerial']}
+              scale={[0.01, 0.01, 0.01]}
+            />
+            <ViroSphere
+              onClick={this.handleUpClick}
+              position={[-0.02, -0.075, -0.2]}
+              materials={['spherematerial']}
+              scale={[0.01, 0.01, 0.01]}
+            />
+          </ViroARCamera>
+        </ViroARScene>
+      </>
+    );
+  }
+
   onPlaneSelected = (anchor) => {
-    const worldCenterPosition = [
+    const anchoredPosition = [
       anchor.position[0] + anchor.center[0],
       anchor.position[1] + anchor.center[1],
       anchor.position[2] + anchor.center[2]
     ];
     this.setState({
-      // planePosition: anchor.position,
-      foundPlane: true,
-      planeWidth: anchor.width,
-      planeLength: anchor.height,
-      planeCenter: anchor.center,
-      planeRotation: anchor.rotation,
-      planePosition: worldCenterPosition
+      planePosition: anchoredPosition
     });
     this.arPlaneRef.setNativeProps({ pauseUpdates: true });
   };
 
   handleUpClick = (pos) => {
-    this.forceRef.applyImpulse([0, 0, -0.5], pos);
+    this.playerRef.applyImpulse([0, 0, -0.5], pos);
   };
 
   handleLeftClick = (pos) => {
-    this.forceRef.applyImpulse([-0.5, 0, 0], pos);
+    this.playerRef.applyImpulse([-0.5, 0, 0], pos);
   };
 
   handleRightClick = (pos) => {
-    this.forceRef.applyImpulse([0.5, 0, 0], pos);
+    this.playerRef.applyImpulse([0.5, 0, 0], pos);
   };
 
   handleDownClick = (pos) => {
-    this.forceRef.applyImpulse([0, 0, 0.5], pos);
+    this.playerRef.applyImpulse([0, 0, 0.5], pos);
   };
 
-  generatePlayer = () => {
-    const { planeLength } = this.state;
+  resetPlayer = () => {
     const physicsBody = {
       type: 'Dynamic',
       mass: 4,
@@ -84,8 +131,27 @@ export default class ARView extends Component {
         params: [0.14]
       }
     };
-    // const playerStartPosition = planeLength - planeLength - planeLength / 2 + 0.1;
+    TimerMixin.setTimeout(() => {
+      this.playerRef.setNativeProps({ physicsBody: null });
+      this.playerRef.setNativeProps({ position: [0, 0.1, 0] });
+      TimerMixin.setTimeout(() => {
+        this.playerRef.setNativeProps({ physicsBody });
+      });
+    });
+  };
 
+  generatePlayer = () => {
+    const physicsBody = {
+      type: 'Dynamic',
+      mass: 4,
+      useGravity: true,
+      enabled: true,
+      velocity: [0, 0, 0],
+      shape: {
+        type: 'Sphere',
+        params: [0.14]
+      }
+    };
     return (
       <Viro3DObject
         position={[0, 0.1, 0]}
@@ -96,77 +162,10 @@ export default class ARView extends Component {
         type="VRX"
         renderingOrder={0}
         physicsBody={physicsBody}
-        ref={obj => (this.forceRef = obj)}
+        ref={obj => (this.playerRef = obj)}
       />
     );
   };
-
-  render() {
-    const { planePosition } = this.state;
-
-    return (
-      <ViroARScene
-        onTrackingUpdated={this.onInitialized}
-        physicsWorld={{
-          gravity: [0, -9.81, 0]
-        }}
-      >
-        <ViroAmbientLight color="#ffffff" />
-        <ViroARPlane
-          onPlaneSelected={this.onPlaneSelected}
-          ref={component => (this.arPlaneRef = component)}
-        >
-          <ViroNode position={planePosition}>
-            <ViroQuad
-              position={[0, 0, -0.3]}
-              scale={[1.5, 1.5, 1.5]}
-              rotation={[-90, 0, 0]}
-              physicsBody={{ type: 'Static', restitution: 0.75, friction: 0.7 }}
-              materials="ground"
-              renderingOrder={-1}
-            />
-            <ViroQuad
-              key="deadSpace"
-              // onCollision={this.generatePlayer}
-              height={100}
-              width={100}
-              rotation={[-90, 0, 0]}
-              position={[0, -1, 0]}
-              materials={['transparent']}
-              physicsBody={{ type: 'Static' }}
-            />
-            <ViroNode position={planePosition}>{this.generatePlayer()}</ViroNode>
-          </ViroNode>
-        </ViroARPlane>
-        <ViroARCamera active position={[0, 0, -0.2]}>
-          <ViroSphere
-            onClick={this.handleLeftClick}
-            position={[-0.04, -0.05, -0.2]}
-            materials={['spherematerial']}
-            scale={[0.01, 0.01, 0.01]}
-          />
-          <ViroSphere
-            onClick={this.handleRightClick}
-            position={[-0.04, -0.1, -0.2]}
-            materials={['spherematerial']}
-            scale={[0.01, 0.01, 0.01]}
-          />
-          <ViroSphere
-            onClick={this.handleDownClick}
-            position={[-0.055, -0.075, -0.2]}
-            materials={['spherematerial']}
-            scale={[0.01, 0.01, 0.01]}
-          />
-          <ViroSphere
-            onClick={this.handleUpClick}
-            position={[-0.02, -0.075, -0.2]}
-            materials={['spherematerial']}
-            scale={[0.01, 0.01, 0.01]}
-          />
-        </ViroARCamera>
-      </ViroARScene>
-    );
-  }
 }
 
 ViroMaterials.createMaterials({
@@ -175,12 +174,6 @@ ViroMaterials.createMaterials({
   },
   spherematerial: {
     diffuseColor: 'rgb(19,42,143)'
-  },
-  metal: {
-    lightingModel: 'Lambert',
-    diffuseTexture: planeDiffuse,
-    normalTexture: planeNormal,
-    specularTexture: planeSpecular
   },
   ground: {
     diffuseColor: '#007CB6E6'
