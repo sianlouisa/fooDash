73d707e0b04211c0081b5c7063d736b3c71d9af6
Nawaar Patel <nawaarpatel@Nawaars-MacBook-Pro.local>
2019-01-24 10:06:08 +0000

    Restructuring screens and components. Also created updatescore api

--- js/Screens/StartScreen.js
+++ js/Screens/StartScreen.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 // import PropTypes from 'prop-types';
-import PlayButton from './PlayButton';
+import PlayButton from '../Components/PlayButton';
 
 class StartScreen extends Component {
   state = {}
