c196c16a5723ac89657a04bce6f18de9367c4593
brlom <brlom007@gmail.com>
2019-01-28 12:49:57 +0000

    adding winner message when hitting safe zone

--- js/api.js
+++ js/api.js
@@ -1,4 +1,5 @@
-import * as firebase from 'firebase';
+// import * as firebase from 'firebase';
+import firebase from '@firebase/app';
 import { firebaseConfig } from '../config';
 import '@firebase/firestore';
 
