66a0266938c18a4c647977cf4a709c48eefe8970
john12321 <johnomeara72@gmail.com>
2019-01-29 15:47:59 +0000

    created main app logo with basic styling

---
+++ js/Components/Logo.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Image, View, StyleSheet } from 'react-native';
+import logo from '../res/assets/foodDashLogo1.png';
+
+const Logo = () => {
+  return (
+    <View style={styles.container}>
+      <Image style={styles.logo} source={logo} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  logo: {
+    width: 90,
+    height: 90,
+  }
+
+});
+
+export default Logo;
